783c89fa0e2e9dc4ebe726f4f2678e5bf689a4c4
dwyfrequency <jdwy215@gmail.com>
2018-07-30 07:05:18 -0400

    adding state to our object

--- chapter-3/react-complete-guide/src/App.js
+++ chapter-3/react-complete-guide/src/App.js
@@ -1,21 +1,47 @@
-/* Even though in our first iteration of this code, we did not explicit use React, we still need to have it b/c React.createElement is called behind the scenes. 
-Now we have it in our code explicitly called */
 import React, { Component } from "react";
 import "./App.css";
-// important to have uppercase name, b/c in jsx all elements starting with lowercase characters are reserved for the native html elements like h1, or p
-import Person from "./Person/Person"; // relative path, we can drop the .js b/c it will be added in our build workflow
+import Person from "./Person/Person";
 
 class App extends Component {
+  // managed from inside a component that extends Component, not available in function components
+  state = {
+    persons: [
+      {
+        name: "Max",
+        age: 28
+      },
+      {
+        name: "Tom",
+        age: 40
+      },
+      {
+        name: "steph",
+        age: 21
+      }
+    ]
+  };
+
   render() {
-    // cannot use class, must use className as class is a reserve word
-    // our jsx must have one root element, we should wrap it in one root component
-    // the () are so we can structure the jsx along multiple lines
+    // to add dynamic content, we pass props
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        {/* can either have opening and closing tags or be self closing like the below  */}
-        <Person />
+        <button>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        >
+          blemmmer
+        </Person>
       </div>
     );
   }
--- chapter-3/react-complete-guide/src/Person/Person.js
+++ chapter-3/react-complete-guide/src/Person/Person.js
@@ -2,8 +2,18 @@
 // we need to import react b/c it calls react create element behind the scenes
 import React from "react";
 
-const person = () => {
-  return <p>I'm a Person!</p>;
+// when adding dynamic content, we must wrap it in {}
+// we will receive one arg for our function by default, should name props
+const person = props => {
+  return (
+    <div>
+      <p>
+        I'm {props.name} and I am {props.age} years old
+      </p>
+      {/* automatically passed from react when there is nested elements in this case a text element */}
+      <p>{props.children}</p>
+    </div>
+  );
 };
 
 // export person const
