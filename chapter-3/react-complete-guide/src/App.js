77a4c1c9f798f269ce6ce28682e2a5704cec9c23
dwyfrequency <jdwy215@gmail.com>
2018-08-02 07:26:41 -0400

    setting styles dyanmically

--- chapter-3/react-complete-guide/src/App.js
+++ chapter-3/react-complete-guide/src/App.js
@@ -54,7 +54,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
+      color: "white",
       font: "inherit",
       border: "1px solid blue",
       boxShadow: "0 2px 2px grey",
@@ -82,6 +83,7 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = "red";
     }
 
     return (
