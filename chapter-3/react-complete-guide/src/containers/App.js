e9917871a7480a7b073eb921a2dda703fb0ae985
dwyfrequency <jdwy215@gmail.com>
2018-08-10 08:13:53 -0400

    adding higher order in sub components

--- chapter-3/react-complete-guide/src/containers/App.js
+++ chapter-3/react-complete-guide/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from "react";
 import classes from "./App.css";
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
+import WithClass from "../hoc/WithClass";
 
 class App extends PureComponent {
   constructor(props) {
@@ -97,7 +98,7 @@ class App extends PureComponent {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button
           onClick={() => {
             this.setState({ showPersons: true });
@@ -105,15 +106,14 @@ class App extends PureComponent {
         >
           Show Persons
         </button>
-        <Cockpit
-          // title is passed from index.js
+        <Cockpit // title is passed from index.js
           title={this.props.title}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonHandler}
         />
         {persons}
-      </div>
+      </WithClass>
     );
   }
 }
