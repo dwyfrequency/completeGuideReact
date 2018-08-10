d2a501b4d4b50d0f08bff6e60609da67b6f57433
dwyfrequency <jdwy215@gmail.com>
2018-08-10 07:29:33 -0400

    adding higher order component

--- chapter-3/react-complete-guide/src/components/Cockpit/Cockpit.js
+++ chapter-3/react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,10 +1,11 @@
 import React from "react";
 import classes from "./Cockpit.css";
+import Aux from "../../hoc/Aux";
 
 const cockpit = props => {
-  let btnClass = "";
+  let btnClass = classes.Button;
   if (props.showPersons) {
-    btnClass = classes.Red;
+    btnClass = [classes.Button, classes.Red].join(" ");
   }
 
   const assignedClasses = [];
@@ -14,15 +15,19 @@ const cockpit = props => {
   if (props.persons.length <= 1) {
     assignedClasses.push(classes.bold);
   }
+  /* Instead of just having this wrapping div, we can create a higher order function (in hoc directory)
+  Ex. Aux component below. The hocs are not representational, they wrap other components to add a certain functionality.
+  Now, we dont need a wrapping div which can help maintain our styling if we are using something like flexbox
+  */
   return (
-    <div className={classes.Cockpit}>
+    <Aux>
       <h1>Hi, I'm a React App</h1>
       <h3>{props.title}</h3>
       <p className={assignedClasses.join(" ")}>This is really working!</p>
       <button className={btnClass} onClick={props.clicked}>
         Toggle Persons
       </button>
-    </div>
+    </Aux>
   );
 };
 
---
+++ chapter-3/react-complete-guide/src/hoc/Aux.js
@@ -0,0 +1,4 @@
+// this will simply return whatever is nested below it
+const aux = props => props.children;
+
+export default aux;
