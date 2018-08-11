a2e803f54ed06846a47248e80af5518734a963aa
dwyfrequency <jdwy215@gmail.com>
2018-08-11 06:28:03 -0400

    switched hoc to stateful

--- chapter-3/react-complete-guide/src/hoc/withClass_V2.js
+++ chapter-3/react-complete-guide/src/hoc/withClass_V2.js
@@ -1,11 +1,27 @@
-import React from "react";
+// import React from "react";withClass_V2
+import React, { Component } from "react";
 
+// we can use lower case b/c it is not a component, we are just returning a function
+// const withClass_V2 = (WrappedComponent, className) => {
+//   return props => (
+//     <div className={className}>
+//       {/* we use the spread operator to take the props object and create a new object of key value pairs */}
+//       <WrappedComponent {...props} />
+//     </div>
+//   );
+// };
+
+// stateful version of the above - we have anonomous class below
 const withClass_V2 = (WrappedComponent, className) => {
-  return props => (
-    <div className={className}>
-      <WrappedComponent />
-    </div>
-  );
+  return class extends Component {
+    render() {
+      return (
+        <div className={className}>
+          <WrappedComponent {...this.props} />
+        </div>
+      );
+    }
+  };
 };
 
 export default withClass_V2;
