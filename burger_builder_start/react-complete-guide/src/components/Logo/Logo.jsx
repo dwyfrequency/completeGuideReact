7c9c73f347e064b32db2cd6c73b95a3217d68b96
dwyfrequency <jdwy215@gmail.com>
2018-08-16 08:31:57 -0400

    adding logo img and css

---
+++ burger_builder_start/react-complete-guide/src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+/* we import the image b/c our build workflow bundles our src folder and creates a new output folder. Our whole scr folder will be shipped so we need to make webpack aware of this image so webpack can bundle it accordingly. So we need to import it 
+We then just provide our img tag with the imported module and webpack will give it the optimized image 
+*/
+import burgerLogo from "../../assets/images/burger-logo.png";
+
+const logo = props => (
+  <div>
+    <img src={burgerLogo} alt="MyBurger" />
+  </div>
+);
+
+export default logo;
