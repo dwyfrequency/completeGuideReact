998e44dc3cbd20d5c632197c39c81e3ac0198235
dwyfrequency <jdwy215@gmail.com>
2018-08-18 08:45:03 -0400

    adding 3 stacks icon

--- burger_builder_start/react-complete-guide/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.jsx
+++ burger_builder_start/react-complete-guide/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.jsx
@@ -3,8 +3,10 @@ import Logo from "../../../Logo/Logo.jsx";
 import classes from "./DrawerToggle.css";
 
 const drawerToggle = props => (
-  <div onClick={props.clicked} className={classes.Logo}>
-    <Logo />
+  <div onClick={props.clicked} className={classes.DrawerToggle}>
+    <div />
+    <div />
+    <div />
   </div>
 );
 
