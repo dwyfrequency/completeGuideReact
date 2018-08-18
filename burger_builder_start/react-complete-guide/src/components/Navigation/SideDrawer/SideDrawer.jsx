f3a076e5d5ea31eaabd41dfe3e6ac178d98704e5
dwyfrequency <jdwy215@gmail.com>
2018-08-18 06:44:56 -0400

    adding functionality to remove side drawler from screen on click

--- burger_builder_start/react-complete-guide/src/components/Navigation/SideDrawer/SideDrawer.jsx
+++ burger_builder_start/react-complete-guide/src/components/Navigation/SideDrawer/SideDrawer.jsx
@@ -6,10 +6,16 @@ import Backdrop from "../../UI/Backdrop/Backdrop.jsx";
 import Aux from "../../../hoc/Aux";
 
 const sideDrawer = props => {
+  // adding logic to move in or out the sidedrawer
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
+
   return (
     <Aux>
       <Backdrop show={props.open} clicked={props.closed} />
-      <div className={classes.SideDrawer}>
+      <div className={attachedClasses.join(" ")}>
         {/* we can use the logo class b/c css modules will convert our classes into scoped names. 
       They will be converted to separate classes so they dont interfere  */}
         <div className={classes.Logo}>
