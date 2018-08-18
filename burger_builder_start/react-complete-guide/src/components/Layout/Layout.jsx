2b082871d332491f1cc0e913e2547258d457ba30
dwyfrequency <jdwy215@gmail.com>
2018-08-18 06:38:50 -0400

    adding props to remove backdrop for sidedrawer and fixing typo

--- burger_builder_start/react-complete-guide/src/components/Layout/Layout.jsx
+++ burger_builder_start/react-complete-guide/src/components/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar.jsx";
@@ -7,12 +7,27 @@ import SideDrawer from "../Navigation/SideDrawer/SideDrawer.jsx";
 /* we can't return just adjacent jsx elements. 
 we have three options return an array, create a higher order component to wrap it, or wrap it in a div */
 
-const layout = props => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  };
 
-export default layout;
+  SideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.SideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
--- burger_builder_start/react-complete-guide/src/components/Navigation/SideDrawer/SideDrawer.jsx
+++ burger_builder_start/react-complete-guide/src/components/Navigation/SideDrawer/SideDrawer.jsx
@@ -2,19 +2,24 @@ import React from "react";
 import Logo from "../../Logo/Logo.jsx";
 import NavigationItems from "../NavigationItems/NavigationItems.jsx";
 import classes from "./SideDrawer.css";
+import Backdrop from "../../UI/Backdrop/Backdrop.jsx";
+import Aux from "../../../hoc/Aux";
 
 const sideDrawer = props => {
   return (
-    <div className={classes.SideDrawer}>
-      {/* we can use the logo class b/c css modules will convert our classes into scoped names. 
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={classes.SideDrawer}>
+        {/* we can use the logo class b/c css modules will convert our classes into scoped names. 
       They will be converted to separate classes so they dont interfere  */}
-      <div className={classes.Logo}>
-        <Logo />
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
