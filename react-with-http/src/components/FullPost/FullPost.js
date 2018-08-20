e79992d9f2e4a98b2bcaa1c8802e578e5be4cc2e
dwyfrequency <jdwy215@gmail.com>
2018-08-20 06:15:18 -0400

    adding post loading for fullpost

--- react-with-http/src/components/FullPost/FullPost.js
+++ react-with-http/src/components/FullPost/FullPost.js
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import "./FullPost.css";
 
 class FullPost extends Component {
+  state = {
+    loadedPost: null
+  };
+  componentDidUpdate = (prevProps, prevState) => {
+    if (this.props.id) {
+      axios
+        .get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+        .then(resp => {
+          this.setState({ loadedPost: resp.data });
+        });
+    }
+  };
+
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
     if (this.props.id) {
+      // we need to do this check and the one below, b/c once we get the postId, our get request is triggered.
+      // It takes time to receive it back so we dont want to try and render undefined data if there is a timing delay
+      let post = <p style={{ textAlign: "center" }}>Loading...!</p>;
+    }
+    if (this.state.loadedPost) {
       post = (
         <div className="FullPost">
-          <h1>Title</h1>
-          <p>Content</p>
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
             <button className="Delete">Delete</button>
           </div>
