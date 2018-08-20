d71bdf53297df2cd37293d036f5808addeed0021
dwyfrequency <jdwy215@gmail.com>
2018-08-20 07:39:28 -0400

    adding initial annotations for interceptors

--- react-with-http/src/index.js
+++ react-with-http/src/index.js
@@ -5,9 +5,14 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import axios from "axios";
 
+/* interceptors are defined globally. You can intercept requests or responses before they are handled by then or catch.
+Common use case is to insert common headers
+ */
 axios.interceptors.request.use(
   request => {
+    // commonly, we might edit the request before sending it
     console.log(request);
+    // must return the request, else our promise will never get it ie. we're blocking it
     return request;
   },
   error => {
