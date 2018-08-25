ef1cb0f589cb3b09923e7636aef4547eef667dff
dwyfrequency <jdwy215@gmail.com>
2018-08-25 11:28:21 -0400

    adding route path for rendering components in spa

--- react-with-http/src/containers/Blog/Blog.js
+++ react-with-http/src/containers/Blog/Blog.js
@@ -5,6 +5,7 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import "./Blog.css";
 import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
 
 class Blog extends Component {
   render() {
@@ -17,18 +18,23 @@ class Blog extends Component {
                 <a href="/">Home</a>
               </li>
               <li>
-                <a href="/">New Post</a>
+                <a href="/new-post">New Post</a>
               </li>
             </ul>
           </nav>
         </header>
-        <Route />
-        {/* <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section> */}
+        {/* define path and what should render when it is hit
+        react router to determine what path you are on, sees if your current path starts "/" in this case 
+        with  exact dictates that the complete path must be exactly that */}
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+        <Route path="/" render={() => <h1>New Post</h1>} /> */}
+        {/* component property allows use to pass a component that should be rendered in this
+        routes place; 
+        it needs to be a reference to the function or class that we want to use 
+        With the below, we can see all of the posts render for the home path
+        - note, we are not passing a string, we are passing the actual imported function or component*/}
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
